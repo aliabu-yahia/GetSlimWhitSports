743ce4321c6f3e9fff2b5d80b1cc5f552df3bbe1
aliabu-yahia <abuyahiaali21@gmail.com>
2021-01-27 14:37:57 +0200

    to add js file

---
+++ app.js
@@ -0,0 +1,38 @@
+// hi my name is ali khames
+/* 
+i live in amman
+i am a mechanical enginering
+*/
+console.log('hello hello in lab 04');
+console.log(2**8);
+
+var x = 6 ;
+console.log(x);
+console.log(x+=2);
+console.log(x);
+// x have a new value
+
+// in comparison operators the answer while only true or false like :
+console.log(x==8)
+console.log(x!=8)
+
+alert('choose form 1 to 100');
+var grade = prompt('what is your grade?');
+console.log(grade);
+
+if (grade > 90){
+    message =  'exellent';
+
+}else if ( grade > 60){
+        message = 'very good';
+
+}else if ( grade > 60){
+    message = 'good';
+}else if (grade>=50){
+    message = 'you pass but you should do beter';
+}else{
+    message = 'you fall you should study harder';
+}
+
+document.write('<h3>' + message +'</h3>' );
+
