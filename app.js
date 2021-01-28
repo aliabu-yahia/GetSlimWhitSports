f598d34fa8faa70c8f60f0688bd57f1603c7a480
aliabu-yahia <abuyahiaali21@gmail.com>
2021-01-28 15:59:20 +0200

    to add for ststment

--- app.js
+++ app.js
@@ -16,23 +16,40 @@ console.log(x);
 console.log(x==8)
 console.log(x!=8)
 
-alert('choose form 1 to 100');
-var grade = prompt('what is your grade?');
-console.log(grade);
 
-if (grade > 90){
-    message =  'exellent';
+var sports = prompt('What is your best sport?');
 
-}else if ( grade > 60){
-        message = 'very good';
-
-}else if ( grade > 60){
-    message = 'good';
-}else if (grade>=50){
-    message = 'you pass but you should do beter';
-}else{
-    message = 'you fall you should study harder';
+while(sports !=='football' && sports!=='bascetball' && sports!=='tennis'){
+    sports =prompt('please choose one of football, bascetball or tennis ')
 }
 
-document.write('<h3>' + message +'</h3>' );
+var star = prompt('how many stars you give this website?');
+
+while(star > 5 ){
+    star =prompt('please choose number from 1 to 5 ')
+}
+
+
+var massage = ''
+var result = ''
+
+if (sports ===  'football'){
+    message =  'exellent chooes';
+    result = '<img width="290" src="https://www.thestatesman.com/wp-content/uploads/2020/04/1c5b1aa3386eeb2c21d633f04e2ddfbe.jpg" />';
+
+}else if ( sports === 'bascetball'){
+        message = ' are you tall enough? ';
+        result = '<img width="290" src="https://www.history.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTU3ODc4NjAzNTE3NDcwMDI1/ask-history-who-invented-basketball-istock_000006523151large-2.jpg" />';
+}else{
+    message = 'its boring sports';
+    result = '<img width="290" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwFi_DbgIBjzfUjSWUigKIWQxLKDkhXd3-6A&usqp=CAU" />';
+}
+
+document.write('<br>' + message );
+
+document.write( result );
+
+for(var i = 1 ; i<= star ; i++){
+    document.write('<br>'+'* ')};
+    
 
